fc49b2383799abfe7290992ab40cca612f526017
Souvik Santra <40638277+s01k@users.noreply.github.com>
2021-04-13 10:48:22 +0530

    v1.2
    cookies, <li>

--- j/drk.js
+++ j/drk.js
@@ -1,14 +1,30 @@
+$(document).ready(function(e) {  
+  // dark mode switch
+  var setTheme = function (theme) {
+    if (theme === 'dark') {
+    // dark
+    $( "body" ).removeClass("standard");
+    $( "body" ).addClass( "dark" );
+    $(".inner-switch").text("Light");
+    setCookie('Theme', 'dark', 30);
+    } else {
+    $("body").removeClass("dark");
+    $("body").addClass("standard");
+    $(".inner-switch").text("Dark");
+    setCookie('Theme', 'standard', 30);
+    }
+    };
+    
+    currentTheme = getCookie('Theme');
+    setTheme(currentTheme);
 
-
-$(document).ready(function(e) {   
     $( ".inner-switch" ).on("click", function() {
-        if( $( "body" ).hasClass( "dark" )) {
-          $( "body" ).removeClass( "dark" );
-          $( ".inner-switch" ).text( "Dark" );
-        } else {
-          $( "body" ).addClass( "dark" );
-          $( ".inner-switch" ).text( "Light" );
-        }
+      if ($("body").hasClass("dark")) {
+      // standard
+      setTheme('standard');
+      } else {
+      // dark mode
+      setTheme('dark');
+      }
     });
-
  });
